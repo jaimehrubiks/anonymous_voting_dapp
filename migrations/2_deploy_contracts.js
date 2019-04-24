beb6267db1cfad287a0160cd42cef020d0de745d
jaimehrubiks <jaimehidalgo2@hotmail.com>
2019-04-24 12:20:50 -0500

    master constructor admin

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Election = artifacts.require("./Election.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Election);
+  deployer.deploy(Election, "0x0A852248141A937DA7221e50CEF4965e3e072a29");
 };
